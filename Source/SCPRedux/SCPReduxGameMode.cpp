4bad493c8ff3f97f6e9a4c20c4d660af96d17480
connoreccleston <connoreccleston@gmail.com>
2017-05-28 03:28:53 -0600

    Initial commit. Been working on basic UX.

---
+++ Source/SCPRedux/SCPReduxGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "SCPReduxGameMode.h"
+#include "SCPReduxHUD.h"
+#include "SCPReduxCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ASCPReduxGameMode::ASCPReduxGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ASCPReduxHUD::StaticClass();
+}

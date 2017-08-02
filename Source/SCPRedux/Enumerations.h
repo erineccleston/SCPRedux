a6b3c49b1e8559eefcc98c0c2610bb76ddfa8c9f
connoreccleston <connoreccleston@gmail.com>
2017-08-01 20:58:42 -0600

    Added rooms table in advance of the new procgen algorithm.

--- Source/SCPRedux/Enumerations.h
+++ Source/SCPRedux/Enumerations.h
@@ -5,6 +5,130 @@
 #include "CoreMinimal.h"
 
 UENUM(BlueprintType)
+enum class ERoomZones : uint8
+{
+	Special,
+	Light,
+	Heavy,
+	Entrance
+};
+
+UENUM(BlueprintType)
+enum class ERoomShapes : uint8
+{
+	Special,
+	Endcap,
+	Hallway,
+	Corner,
+	Threeway,
+	Fourway
+};
+
+UENUM(BlueprintType)
+enum class ERooms : uint8
+{
+	NoRoom,
+	LCZRoom,
+	HCZRoom,
+	EZRoom,
+
+	lockroom,
+	room173,
+	start,
+	room178,
+	room1123,
+	room1archive,
+	room2storage,
+	room3storage,
+	room2tesla_lcz,
+	endroom,
+	room012,
+	room205,
+	room2,
+	room2_3,
+	room2_2,
+	room2C,
+	room2c2,
+	room2closets,
+	room2elevator,
+	room2doors,
+	room2scps,
+	room860,
+	room2testroom2,
+	room3,
+	room3_2,
+	room4,
+	roompj,
+	room914,
+	room2gw,
+	room2gw_b,
+	room1162,
+	room2scps2,
+	room2sl,
+	room2_4,
+	room2_5,
+	lockroom3,
+	room4_2,
+	checkpoint1,
+	room008,
+	coffin,
+	endroom2,
+	testroom,
+	tunnel,
+	tunnel2,
+	room035,
+	room049,
+	room106,
+	room2Ctunnel,
+	room2nuke,
+	room2pipes,
+	room2pit,
+	room3pit,
+	room2servers,
+	room2shaft,
+	room2tunnel,
+	room2tesla_hcz,
+	room3tunnel,
+	room4tunnels,
+	room513,
+	room966,
+	room3z2,
+	room4pit,
+	checkpoint2,
+	exit1,
+	gatea,
+	gateaentrance,
+	medibay,
+	lockroom2,
+	room079,
+	room2z3,
+	room2cafeteria,
+	room2Cz3,
+	room2ccont,
+	room2offices,
+	room2offices2,
+	room2offices3,
+	room2poffices,
+	room2poffices2,
+	room2sroom,
+	room2toilets,
+	room2tesla,
+	room3servers,
+	room3servers2,
+	room3z3,
+	room4z3,
+	room1lifts,
+	room3gw,
+	room2servers2,
+	room3offices,
+	room2z3_2,
+	room2offices4,
+	pocketdimension,
+	dimension1499
+};
+
+// used by the old algorithm
+UENUM(BlueprintType)
 enum class ERoomType : uint8
 {
 	None,
@@ -99,19 +223,19 @@ enum class EDocuments : uint8
 	sn
 };
 
-// Also not used
-UENUM(BlueprintType)
-enum class EItemTypes : uint8
-{
-	Consumable,
-	Effectless,
-	Electronics,
-	Equippable,
-	Headgear,
-	Meta,
-	Readable,
-	Wearable
-};
+//// Also not used
+//UENUM(BlueprintType)
+//enum class EItemTypes : uint8
+//{
+//	Consumable,
+//	Effectless,
+//	Electronics,
+//	Equippable,
+//	Headgear,
+//	Meta,
+//	Readable,
+//	Wearable
+//};
 
 // not being used right now
 UENUM(BlueprintType)
